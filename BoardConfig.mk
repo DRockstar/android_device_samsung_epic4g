1fef975959a4ed1f86bc7ee410b4272ea5f3d9af
DRockstar <djdonovanbartish@yahoo.com>
2011-06-30 04:40:49 -0400

    Indulge 910 model edits
    Change-Id: Id49de6ea219cb90bf1abd90be06359bfc3caea10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,3 +24,9 @@ TARGET_PREBUILT_KERNEL := device/samsung/epic4g/kernel
 BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/epic4g/recovery_ui.c
 #BOARD_HAS_NO_SELECT_BUTTON := true
 
+# custom lun0 file for USB Mass Storage
+BOARD_UMS_LUNFILE := /sys/devices/platform/s3c-usbgadget/gadget/lun0/file
+
+BOARD_BOOT_BML := /dev/block/bml8
+BOARD_RECOVERY_BML := /dev/block/bml9
+
